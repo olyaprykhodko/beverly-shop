0b20b820726dcdda3dcd64de81c868743039e7f5
Olha Prykhodko <o.u.prihodko@gmail.com>
2024-06-18 18:32:37 +0300

    start

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Beverly shop</a>
+      </header>
+      <main>List of products</main>
+    </div>
+  );
+}
+
+export default App;
